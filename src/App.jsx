0b79528ed1c9d240f78ca9c4da7ee6162ab9db3c
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-30 23:13:57 +0530

    feat: question

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css'
 
 const App = () => {
   let url = `https://quizapi.io/api/v1/questions?apiKey=6rH1SDnBsOG7J0sjCKkgTuvC6Q83Zscrto7xbCcX&difficulty=Medium&limit=10`;
+  const [data,setdata] = useState([]); 
 
   useEffect(() => {
-    // Async function to fetch quiz data
+
     const fetchQuiz = async () => {
       try {
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
+        setdata(data);
       } catch (error) {
         console.error("Error fetching quiz data:", error);
       }
@@ -20,8 +23,16 @@ const App = () => {
 
   return (
     <div>
-      <h1>Quiz App</h1>
-      {/* Add your components here */}
+      <div className='quiz-container'>
+        <h1>Quiz App</h1>
+          <div className='quesans-window'>
+            <h4>          {data.length > 0 ? (
+            <h4>{data[0].question}</h4>  
+          ) : (
+            <h4>Loading...</h4>  
+          )}</h4>
+          </div>
+      </div>
     </div>
   );
 };
