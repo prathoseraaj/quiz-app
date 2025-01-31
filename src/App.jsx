598607c70cb64e52ae4fddbfbc57422c51963220
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-31 23:23:57 +0530

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import './App.css'
 const App = () => {
   let url = `https://quizapi.io/api/v1/questions?apiKey=6rH1SDnBsOG7J0sjCKkgTuvC6Q83Zscrto7xbCcX&difficulty=Medium&limit=10`;
   const [data,setdata] = useState([]);
+  const [quenumber, SetQuenumber] = useState(0);
   const [selectanswer, setSelectanswer] = useState(''); 
   const [correctAnswer, setCorrectAnswer] = useState('');
   const [total, setTotal] = useState(0);
+
 
 
   useEffect(() => {
@@ -17,9 +19,9 @@ const App = () => {
         const data = await response.json();
         console.log(data);
         setdata(data);
-        for (let key in data[0]) {
-          if (key.includes("_correct") && data[0][key] === "true") {
-            setCorrectAnswer(key.split('_')[0]);
+        for (let key in data[quenumber]) {
+          if (key.includes("_correct") && data[quenumber][key] === "true") {
+            setCorrectAnswer(key.split('_')[quenumber]);
             console.log(correctAnswer)
           }
         }
@@ -38,7 +40,7 @@ const App = () => {
         <div>
           <div className='ques-window'>
             <h3>          {data.length > 0 ? (
-            <h3>{`Q) ${data[0].question}`}</h3>  
+            <h3>{`Q) ${data[quenumber].question}`}</h3>  
           ) : (
             <h4>Loading...</h4>  
           )}</h3>
@@ -47,34 +49,36 @@ const App = () => {
             <h3  onClick={()=>setSelectanswer('a')} className={selectanswer === 'a'? 'active': 'options'} id='option1'>
               {
                 data.length > 0 ? (
-                  <h3>{`a) ${data[0].answers.answer_a}`}</h3>
+                  <h3>{`a) ${data[quenumber].answers.answer_a}`}</h3>
                 ):<h4>Loding...</h4>
               }
             </h3>
             <h3 onClick={()=>setSelectanswer('b')} className={selectanswer === 'b'? 'active': 'options'} id='option2'>
             {
                 data.length > 0 ? (
-                  <h3>{`b) ${data[0].answers.answer_b}`}</h3>
+                  <h3>{`b) ${data[quenumber].answers.answer_b}`}</h3>
                 ):<h4>Loding...</h4>
               }
             </h3>
             <h3 onClick={()=>setSelectanswer('c')} className={selectanswer === 'c'? 'active': 'options'} id='option3'>
             {
                 data.length > 0 ? (
-                  <h3 >{`c) ${data[0].answers.answer_c}`}</h3>
+                  <h3 >{`c) ${data[quenumber].answers.answer_c}`}</h3>
                 ):<h4>Loding...</h4>
               }
             </h3>
             <h3 onClick={()=>setSelectanswer('d')} className={selectanswer === 'd'? 'active': 'options'} id='option4'>
             {
                 data.length > 0 ? (
-                  <h3>{`d) ${data[0].answers.answer_d}`}</h3>
+                  <h3>{`d) ${data[quenumber].answers.answer_d}`}</h3>
                 ):<h4>Loding...</h4>
               }
             </h3>
           </div>
         </div>
-        <button className='button'>
+        <button className='button'
+                onClick={()=>setQuenumber}
+                >
           Next
         </button>
       </div>
