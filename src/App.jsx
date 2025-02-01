6449bfb438deb5c5acd1df60a4f0223b425f038f
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-01 21:53:45 +0530

    fix: document formation

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 
 const App = () => {
   let url = `https://quizapi.io/api/v1/questions?apiKey=6rH1SDnBsOG7J0sjCKkgTuvC6Q83Zscrto7xbCcX&difficulty=Medium&limit=10`;
-  const [data,setdata] = useState([]);
+  const [data, setdata] = useState([]);
   const [quenumber, setQuenumber] = useState(0);
-  const [selectanswer, setSelectanswer] = useState(''); 
+  const [selectanswer, setSelectanswer] = useState('');
   const [correctAnswer, setCorrectAnswer] = useState('');
   const [total, setTotal] = useState(0);
 
@@ -30,15 +30,15 @@ const App = () => {
       }
     };
 
-    fetchQuiz(); 
-  }, []); 
+    fetchQuiz();
+  }, []);
 
   const handleNextQuestion = () => {
     if (selectanswer === correctAnswer) {
       setTotal(prevTotal => prevTotal + 1);
     }
     setQuenumber(prev => prev + 1);
-    setSelectanswer(''); 
+    setSelectanswer('');
   };
 
 
@@ -46,50 +46,50 @@ const App = () => {
     <div>
       <div className='quiz-container'>
         <h1>Quiz App</h1>
-        {quenumber === 9 ? <h1>{total}</h1>: 
-        <div>
-          <div className='ques-window'>
-            <h3>          {data.length > 0 ? (
-            <h3>{`Q) ${data[quenumber].question}`}</h3>  
-          ) : (
-            <h4>Loading...</h4>  
-          )}</h3>
-          </div>
-          <div className='ans-window'>
-            <h3  onClick={()=>setSelectanswer('a')} className={selectanswer === 'a'? 'active': 'options'} id='option1'>
-              {
-                data.length > 0 ? (
-                  <h3>{`a) ${data[quenumber].answers.answer_a}`}</h3>
-                ):<h4>Loding...</h4>
-              }
-            </h3>
-            <h3 onClick={()=>setSelectanswer('b')} className={selectanswer === 'b'? 'active': 'options'} id='option2'>
-            {
-                data.length > 0 ? (
-                  <h3>{`b) ${data[quenumber].answers.answer_b}`}</h3>
-                ):<h4>Loding...</h4>
-              }
-            </h3>
-            <h3 onClick={()=>setSelectanswer('c')} className={selectanswer === 'c'? 'active': 'options'} id='option3'>
-            {
-                data.length > 0 ? (
-                  <h3 >{`c) ${data[quenumber].answers.answer_c}`}</h3>
-                ):<h4>Loding...</h4>
-              }
-            </h3>
-            <h3 onClick={()=>setSelectanswer('d')} className={selectanswer === 'd'? 'active': 'options'} id='option4'>
-            {
-                data.length > 0 ? (
-                  <h3>{`d) ${data[quenumber].answers.answer_d}`}</h3>
-                ):<h4>Loding...</h4>
-              }
-            </h3>
-          </div>
-          <button className='button' onClick={handleNextQuestion} disabled={!selectanswer}>
-  Next
-</button>
-        </div>  }
-      </div> 
+        {quenumber === 9 ? <h1>{total}</h1> :
+          <div>
+            <div className='ques-window'>
+              <h3>          {data.length > 0 ? (
+                <h3>{`Q) ${data[quenumber].question}`}</h3>
+              ) : (
+                <h4>Loading...</h4>
+              )}</h3>
+            </div>
+            <div className='ans-window'>
+              <h3 onClick={() => setSelectanswer('a')} className={selectanswer === 'a' ? 'active' : 'options'} id='option1'>
+                {
+                  data.length > 0 ? (
+                    <h3>{`a) ${data[quenumber].answers.answer_a}`}</h3>
+                  ) : <h4>Loding...</h4>
+                }
+              </h3>
+              <h3 onClick={() => setSelectanswer('b')} className={selectanswer === 'b' ? 'active' : 'options'} id='option2'>
+                {
+                  data.length > 0 ? (
+                    <h3>{`b) ${data[quenumber].answers.answer_b}`}</h3>
+                  ) : <h4>Loding...</h4>
+                }
+              </h3>
+              <h3 onClick={() => setSelectanswer('c')} className={selectanswer === 'c' ? 'active' : 'options'} id='option3'>
+                {
+                  data.length > 0 ? (
+                    <h3 >{`c) ${data[quenumber].answers.answer_c}`}</h3>
+                  ) : <h4>Loding...</h4>
+                }
+              </h3>
+              <h3 onClick={() => setSelectanswer('d')} className={selectanswer === 'd' ? 'active' : 'options'} id='option4'>
+                {
+                  data.length > 0 ? (
+                    <h3>{`d) ${data[quenumber].answers.answer_d}`}</h3>
+                  ) : <h4>Loding...</h4>
+                }
+              </h3>
+            </div>
+            <button className='button' onClick={handleNextQuestion} disabled={!selectanswer}>
+              Next
+            </button>
+          </div>}
+      </div>
     </div>
   );
 };
